1dba23c40fcdd7384df99f380f265ba81315f49a
Adrián Deccico <adeccico@atlassian.com>
2011-12-23 14:29:36 +1100

    making the utility a package, first commit

---
+++ README.txt
@@ -0,0 +1,12 @@
+===========
+Bellatrix
+===========
+
+Bellatrix is a comprehensive set of tools to automate the management of EC2 ami's and instances.
+
+    * bewitch_ami - Will launch a new instance and execute a configuration script.
+    * set_permissions - Given an ami, this utility will give execution permissions to other accounts.
+    * set_security - Configures an EC2 security group making it possible to only allow incomming connections to certain ports and ip addresses.
+    * start_instance - Starts a new instance from a given ami.
+    * burn_instance - Burn a running instance into a new ami.
+    
---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+version = open('VERSION.txt').read()
+
+
+if sys.version_info <= (2, 4):
+    error = "ERROR: Bellatrix requires Python Version 2.5 or above...exiting."
+    print >> sys.stderr, error
+    sys.exit(1)
+
+
+setup(name='bellatrix',
+      version=version,
+      long_description=open('README.txt').read(),
+      description='Bellatrix is a comprehensive set of tools to automate the management of EC2 ami''s and instances.',
+
+      keywords='ec2, ami, configuration, management, puppet',
+      author='Adrian Deccico',
+      author_email='adeccico@atlassian.com',
+      url='https://bitbucket.org/adeccico/bellatrix',
+      
+      license='Apache License 2.0',
+      
+      packages=['bellatrix'],
+
+      data_files = files,       
+      
+      scripts=['bin/bewitch_ami',
+               'bin/burn_instance',
+               'bin/set_permissions',
+               'bin/set_security',
+               'bin/start_instance', 
+               ],
+
+      platforms = "Posix; MacOS X; Windows",
+
+      #classifiers from http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers = [
+                     'Development Status :: 4 - Beta',
+                     'Environment :: Console',
+                     'Intended Audience :: Developers',
+                     'Intended Audience :: Information Technology',
+                     'Intended Audience :: System Administrators',
+                     'License :: OSI Approved :: Apache Software License',
+                     'Operating System :: OS Independent',
+                     'Programming Language :: Python',
+                     "Topic :: Internet",
+                     'Topic :: System :: Software Distribution',
+                     'Topic :: System :: Systems Administration',
+                     'Topic :: Software Development :: Libraries :: Python Modules',
+                     'Topic :: Utilities',
+                   ],
+      **extra
+      )
