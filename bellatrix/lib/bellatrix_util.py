1281d6af73fdc8257a8ae4a5e9f72bbf73c325ea
Adrián Deccico <deccico@gmail.com>
2012-01-18 00:59:57 +1100

    enhacing error formatting

--- bellatrix/lib/bellatrix_util.py
+++ bellatrix/lib/bellatrix_util.py
@@ -17,23 +17,23 @@ def getConfigFile(path, file_name, description=None):
     if not os.path.isfile(path):
         sys.tracebacklimit = 0 #we don't need the stack trace in this particular case.
         raise Exception(
-                          """\n\n -------------------------- \n """ 
-                          """Error getting configuration file. \n """ 
-                          """%s is looking for the file %s and can not find it.\n """
+                          """\n\n -------------------------- \n""" 
+                          """Error getting configuration file. \n""" 
+                          """%s is looking for the file %s and can not find it.\n"""
                           """Please generate it and try again. %s""" \
                           % (bellatrix.APP, path, "" if description is None else description))
     return open(path)
 
 def getSecret(path=None):
     return getConfigFile(path, bellatrix.SECRET_FILE, \
-                         """Secret Access Key is part of your AWS security credentials.\n """
-                         """Please sign into https://aws-portal.amazon.com/gp/aws/developer/account/index.html?action=access-key"""
+                         """Secret Access Key is part of your AWS security credentials.\n"""
+                         """Please sign into: \nhttps://aws-portal.amazon.com/gp/aws/developer/account/index.html?action=access-key\n"""
                          """in order to get your keys.""")
 
 def getKey(path=None):
     return getConfigFile(path, bellatrix.KEY_FILE, \
-                         """The Access Key Id is part of your AWS security credentials.\n """
-                         """Please sign into https://aws-portal.amazon.com/gp/aws/developer/account/index.html?action=access-key"""
+                         """The Access Key Id is part of your AWS security credentials.\n"""
+                         """Please sign into: \nhttps://aws-portal.amazon.com/gp/aws/developer/account/index.html?action=access-key\n"""
                          """in order to get your keys.""")
     
 def getPrivateKey(path=None):
