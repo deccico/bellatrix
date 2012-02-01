7a2896950e135a9b1595af03a9b5e0fbb9ca7620
Adrian Deccico <adeccico@atlassian.com>
2012-02-01 11:22:02 +1100

    adding start instance utility

--- bellatrix/start_instance.py
+++ bellatrix/start_instance.py
@@ -10,30 +10,24 @@ from bellatrix.lib.ec2_lib import Ec2lib
 from bellatrix.lib import bellatrix_util
 
 class Run():
-    def __init__(self, key, sec, ami, type): #todo move all the globals here
+    def __init__(self, key, sec): 
         self._ec2 = Ec2lib(key, sec) 
-        self.startInstance(ami, type)
 
     def getEc2Instance(self, ami, key_name, security_group, instance_type, instance_initiated_shutdown_behavior="terminate"):
         image = self._ec2.getImage(ami)  
         inst = self._ec2.startInstance(image, key_name, security_group, instance_type, APP, instance_initiated_shutdown_behavior="terminate")
         return inst
 
-    def startInstance(self, ami, type):
-        inst = self.getEc2Instance(ami, "elasticbamboo", ["elasticbamboo"], type)
+    def startInstance(self, ami, type, key_name, security_groups):
+        inst = self.getEc2Instance(ami, key_name, security_groups.split(), type)
         dns_name = self._ec2.getDNSName(inst)
         self._ec2.waitUntilInstanceIsReady(inst)
 
-def run(args):
-    ami = args[1]
-    if len(args) < 3:
-        type = 'm1.large'
-    else:
-        type = args[2]
-    r = Run(KEY, SECRET, APP, ami, type)
+def run(ami, type, key_name, security_groups):
+    r = Run(bellatrix_util.getKey(), bellatrix_util.getSecret())
+    r.startInstance(ami, type, key_name, security_groups)
     return 0
 
 
 if __name__ == '__main__':
-    sys.exit(run(sys.argv))
-
+    sys.exit(run(*sys.argv[1:]))
