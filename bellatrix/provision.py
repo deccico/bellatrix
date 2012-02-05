7279e9e815e3a3daab0aaa0de740052aefb57218
Adrián Deccico <deccico@gmail.com>
2012-02-06 00:29:41 +1100

    enhacing run

--- bellatrix/provision.py
+++ bellatrix/provision.py
@@ -9,6 +9,7 @@ import sys
 
 from bellatrix.lib.ec2_lib import Ec2lib
 from bellatrix.lib import bellatrix_util
+from bellatrix.lib import util
 import bellatrix.bewitch_ami
 
 
@@ -29,7 +30,7 @@ class Provision():
         for ami in amis:
             config_name = ami[1]
             ami = ami[0]
-            r, e = self.executeCommands(user, inst.dns_name, self.pk, commands, config_name)
+            r, e = self.executeCommands(user, dns_name, pk, commands, config_name)
             self.saveReport(r, config_name)
             errors += e
             if len(e) > 0:
@@ -40,42 +41,30 @@ class Provision():
         """execute a configuration"""
         amis_burned = []
         errors = []
-        if config == self.ALL_CONFIGS:
-            configs = self.getConfigs()
+        configs = os.path.splitext(config)[0]
+        cfg = configs
+        sys.path = [util.getCurDir()] + sys.path
+        logging.info("processing: " + cfg + " in: " + os.getcwd())
+        c = __import__(cfg)
+        mod = "c."
+        skip_me = eval(mod + self.SKIP_ME)
+        if skip_me:
+            logging.info("skipping execution of config: %s due to its configuration skip_me=true" % cfg)
         else:
-            configs = [os.path.splitext(config)[0]]
-        sys.path = [bellatrix.lib.util.getCurDir()] + sys.path
-        for cfg in configs:
-            logging.info("processing: " + cfg + " in: " + os.getcwd())
-            if config == self.ALL_CONFIGS:
-                c = __import__(os.path.basename(self.CONFIG_DIR) + "." + cfg)
-                mod = "c." + cfg + "."
-            else:
-                c = __import__(cfg)
-                mod = "c."
-            skip_me = eval(mod + self.SKIP_ME)
-            if skip_me:
-                logging.info("skipping execution of config: %s due to its configuration skip_me=true" % cfg)
-                continue
             amis = eval(mod + self.AMIS)
             commands =  eval(mod + self.CMDS)
             user =  eval(mod + self.USER)
-            burn_at_the_end = eval(mod + self.BURN_OR_NOT)
-            a,e = self._processConfig(amis, commands, user, burn_at_the_end, eval(mod + self.KEY_NAME), 
+            a,e = self._processConfig(amis, commands, user, eval(mod + self.KEY_NAME), 
                                       eval(mod + self.SECURITY_GROUPS), eval(mod + self.INSTANCE_TYPE))
             amis_burned += a
             errors += e
         self.printErrors(errors)
-        logging.info("total of ami's burned:%s" % len(amis_burned))
-        for a in amis_burned:
-            logging.info(str(a))
         return 0 if len(errors)==0 else 1
 
     
-def run(configuration=None):
+def run(configuration):
     r = Provision(bellatrix_util.getKey(), bellatrix_util.getSecret())
-    config = r.ALL_CONFIGS if (not configuration) else configuration
-    exit_code = r.provision(config)
+    exit_code = r.provision(configuration)
     return exit_code
 
 if __name__ == '__main__':
